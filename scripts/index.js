3dece5c4e64569bc419d2b94b8ebd8644b388fbb
Izabela <kolodziejczyk.swift@gmail.com>
2023-02-25 17:55:06 +0100

    init

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+//console.log("Izabela Kołodziejczyk");
+//document.write("Iza");
+
+document.getElementById("name").innerHTML = "Izabela Kołodziejczyk";
+//document.getElementById("lastname").innerHTML = "Kołodziejczyk";
